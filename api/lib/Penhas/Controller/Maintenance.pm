244d5acc6d49ee6dfc3f49a4be0a0957a1a8fe36
Renato Cron <renato.cron@gmail.com>
2021-04-01 14:32:08 -0300

    só rodar cep em prod

--- api/lib/Penhas/Controller/Maintenance.pm
+++ api/lib/Penhas/Controller/Maintenance.pm
@@ -51,29 +51,30 @@ sub housekeeping {
         $r->update({deletion_started_at => \'now()'});
         $ENV{LAST_DELETE_JOB_ID} = $job_id;
     }
-
-    my $update_cep = $c->schema2->resultset('Cliente')->search(
-        {
-            cep_estado => undef,
-        },
-        {
-            columns => ['id'],
-        }
-    );
-    while (my $r = $update_cep->next) {
-
-        my $job_id = $minion->enqueue(
-            'cliente_update_cep',
-            [
-                $r->id,
-            ] => {
-                attempts => 5,
+    if (!is_test()) {
+        my $update_cep = $c->schema2->resultset('Cliente')->search(
+            {
+                cep_estado => undef,
+            },
+            {
+                columns => ['id'],
             }
         );
+        while (my $r = $update_cep->next) {
 
-        slog_info('Adding job cliente_update_cep %s, job id %s', $r->id, $job_id);
-        $r->update({cep_estado => '<minion>'});
-        $ENV{LAST_DELETE_JOB_ID} = $job_id;
+            my $job_id = $minion->enqueue(
+                'cliente_update_cep',
+                [
+                    $r->id,
+                ] => {
+                    attempts => 5,
+                }
+            );
+
+            slog_info('Adding job cliente_update_cep %s, job id %s', $r->id, $job_id);
+            $r->update({cep_estado => '<minion>'});
+
+        }
     }
 
     my $dbh = $c->schema2->storage->dbh;
