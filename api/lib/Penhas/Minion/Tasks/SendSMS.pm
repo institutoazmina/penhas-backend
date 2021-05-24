85d454a96f16ecc629ba47d8029251fe53ef4922
Renato Cron <renato.cron@gmail.com>
2021-05-24 17:00:43 -0300

    fix datetime on lib/Penhas/Minion/Tasks/SendSMS.pm

--- api/lib/Penhas/Minion/Tasks/SendSMS.pm
+++ api/lib/Penhas/Minion/Tasks/SendSMS.pm
@@ -25,7 +25,7 @@ sub send_sms {
             phonenumber => $phonenumber,
             message     => $message,
             notes       => to_json($notes),
-            created_at  => \'NOW(4)',
+            created_at  => \'NOW()',
         }
     );
     log_trace("SentSmsLog", $log->id);
