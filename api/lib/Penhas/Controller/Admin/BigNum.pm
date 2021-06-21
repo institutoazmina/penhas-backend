3b34c1f725dc4581401e5d3bddd5977f65ebca3a
Renato CRON <renato.cron@gmail.com>
2021-06-21 15:03:52 -0300

    metabase no menu "Dashboard"

--- api/lib/Penhas/Controller/Admin/BigNum.pm
+++ api/lib/Penhas/Controller/Admin/BigNum.pm
@@ -6,6 +6,8 @@ use Penhas::Utils;
 use DateTime;
 use MooseX::Types::Email qw/EmailAddress/;
 use Time::HiRes qw/tv_interval gettimeofday/;
+use Crypt::JWT qw(encode_jwt decode_jwt);
+use Mojo::URL;
 
 sub abignum_get {
     my $c = shift;
@@ -34,14 +36,39 @@ sub abignum_get {
     }
     $c->stash(elapsed => tv_interval($t0));
 
+    my @rows            = ({name => 'Panel geral', resource => {dashboard => 4}, params => {}});
+    my $metabase_secret = $ENV{METABASE_SECRET} || 'secret';
+    my @ret             = ();
+    foreach my $payload (@rows) {
+        $payload->{_}{admin_user} = $c->stash('admin_user')->id;
+        $payload->{exp} = time() + 3600;                           # 1 hour
+
+        my $jwt = encode_jwt(
+            alg     => 'HS256',
+            key     => $metabase_secret,
+            payload => $payload
+        );
+        my $url = Mojo::URL->new('https://analytics.penhas.com.br/');
+        $url->path('/embed/dashboard/' . $jwt);
+        $url->fragment('bordered=false&titled=false');
+
+        push @ret, {
+            name => $payload->{name},
+            url  => $url->to_string(),
+        };
+    }
+
     return $c->respond_to_if_web(
         json => {
             json => {
                 results => \@results,
+                reports => \@ret,
             }
         },
         html => {
             results => \@results,
+            reports => \@ret,
+
         },
     );
 }
