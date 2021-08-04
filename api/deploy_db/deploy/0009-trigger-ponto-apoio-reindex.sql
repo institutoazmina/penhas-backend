28c751a03afcf7acfeb8176b29dede4a24eec6a7
Renato CRON <renato.cron@gmail.com>
2021-08-04 09:49:19 -0300

    fix trigger for pg10

--- api/deploy_db/deploy/0009-trigger-ponto-apoio-reindex.sql
+++ api/deploy_db/deploy/0009-trigger-ponto-apoio-reindex.sql
@@ -1,6 +1,7 @@
 -- Deploy penhas:0009-trigger-ponto-apoio-reindex to pg
 -- requires: 0008-postgis
 
+
 BEGIN;
 
 CREATE OR REPLACE FUNCTION ft_ponto_apoio_reindex_all ()
@@ -21,18 +22,25 @@ CREATE TRIGGER trigger_ponto_apoio_projeto_inserted
     FOR EACH STATEMENT
     EXECUTE PROCEDURE ft_ponto_apoio_reindex_all ();
 
-CREATE OR REPLACE FUNCTION f_tgr_quiz_config_after_update ()
-    RETURNS TRIGGER
-    AS $body$
+CREATE OR REPLACE FUNCTION public.f_tgr_quiz_config_after_update() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
 BEGIN
-    update questionnaires
-     set modified_on = now()
-     where id = NEW.questionnaire_id OR id = OLD.questionnaire_id;
+
+    IF (TG_OP = 'UPDATE') THEN
+
+        update questionnaires
+         set modified_on = now()
+         where id = NEW.questionnaire_id OR  id = OLD.questionnaire_id;
+     ELSIF (TG_OP = 'INSERT') THEN
+        update questionnaires
+         set modified_on = now()
+         where id = NEW.questionnaire_id;
+     END IF;
+
     RETURN NEW;
 END;
-
-$body$
-LANGUAGE plpgsql;
+$$;
 
 CREATE TRIGGER tgr_on_quiz_config_after_update
     AFTER INSERT OR UPDATE OR DELETE ON quiz_config
