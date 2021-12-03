ea7c40676a5468bfdc30f880afe5ae71ef9afa96
Renato Cron <renato.cron@gmail.com>
2021-12-03 17:33:59 -0300

    Remover telefone da timeline, exceto admins

--- api/lib/Penhas/Helpers/Timeline.pm
+++ api/lib/Penhas/Helpers/Timeline.pm
@@ -277,7 +277,8 @@ sub add_tweet {
         }
     );
 
-    use DDP; p $reply_to;
+    use DDP;
+    p $reply_to;
 
     if ($reply_to) {
 
@@ -601,9 +602,12 @@ sub _format_tweet {
 
             (is_test() ? (tweet_depth_test_only => $me->{tweet_depth}) : ())
         },
-        id               => $me->{id},
-        content          => $me->{disable_escape}  ? $me->{content} : linkfy(nl2br(xml_escape($me->{content}))),
-        anonimo          => $anonimo && !$eh_admin ? 1              : 0,
+        id      => $me->{id},
+        content => $me->{disable_escape}
+        ? $me->{content}
+        : &_linkfy(&_nl2br(xml_escape(&_remove_phone_number($me->{content})))),
+        anonimo => $anonimo && !$eh_admin ? 1 : 0,
+
         qtde_likes       => $me->{qtde_likes},
         qtde_comentarios => $me->{qtde_comentarios},
         media            => $media_ref,
@@ -618,6 +622,20 @@ sub _format_tweet {
         ($anonimo && !$eh_admin ? (cliente_id => 0) : (cliente_id => $me->{cliente_id})),
 
     };
+}
+
+sub _replace_number {
+    my ($content) = @_;
+    $content =~ s/\d/*/g;
+    return $content;
+}
+
+sub _remove_phone_number {
+    my ($content) = @_;
+
+    $content =~ s/((?:\(?\d{2}\)?\s*)?\b\s*\d{4,5}-?\d{4}\s)/&_replace_number($1)/ge;
+
+    return $content;
 }
 
 sub _gen_uniq_media_url {
