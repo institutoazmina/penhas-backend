df3d95ce76efaaf4dc3f302ec0ef94a667f5fdd2
Renato Cron <renato.cron@gmail.com>
2023-08-14 14:49:43 +0000

    $q is not public

--- api/lib/Penhas/Helpers/Quiz.pm
+++ api/lib/Penhas/Helpers/Quiz.pm
@@ -733,7 +733,7 @@ sub _render_question {
             $public->{$k} = $k =~ /content/ ? tt_render($v, $vars) : $v;
 
             if ($ENV{DEBUG_QUIZ} && $k eq 'content') {
-                $public->{$k} .= ' (' . $public->{code} . ')';
+                $public->{$k} .= ' (' . $q->{code} . ')';
             }
 
         }
